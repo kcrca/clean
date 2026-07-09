0fa4fbe8cbd04a8111727bb00573383fed97e332
Ken Arnold <arnold@peak.im>
2026-07-08 17:05:45 -0700

    add default auto handling for oit files

---
+++ bin/oit.py
@@ -0,0 +1,16 @@
+"""
+OIt files are usually copies of the non-oit variants, this just copies those.
+"""
+from pathlib import Path
+
+import glob
+
+import clip
+
+src_blocks = Path(clip.directory('defaults', 'textures', 'block'))
+dst_blocks = Path(clip.directory('textures', 'block'))
+for src in src_blocks.glob(f'*_oit*.png'):
+    plain = dst_blocks / src.name.replace('_oit', '')
+    if plain.is_file():
+        dst = dst_blocks / src.name
+        plain.copy(dst)
