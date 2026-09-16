13cf302123422dad56aef9d6013b6aef157393ff
Ken Arnold <arnold@peak.im>
2026-09-15 20:36:22 -0700

    check that all paintings are in index.html, and all the ones in index.html are in the images.

--- bin/painting.py
+++ bin/painting.py
@@ -6,14 +6,27 @@
 #     (*) Broken-out images of each individual painting for the site.
 __author__ = 'arnold'
 
+from pathlib import Path
+
 import glob
 import json
 import os
-from pathlib import Path
-
+import sys
+from bs4 import BeautifulSoup
 from PIL import Image
 
 import clip
+
+site = Path(clip.directory('site'))
+index = BeautifulSoup(open(site / 'index.html').read(), 'html.parser')
+srcs = set()
+for img in index.find_all('img'):
+    src_img = img.get('src')
+    # 'back.png' is the image used for the backs of paintings
+    if 'painting' in src_img and not src_img.endswith('back.png'):
+        srcs.add(src_img.replace('painting/', ''))
+unused_srcs = set(srcs)
+missing_srcs = set()
 
 # If this ever changes it could be worth the trouble to generalize, but for now I'm
 # just assuming the structure of the images.
@@ -35,6 +48,11 @@ art_imgs = []
 for img_file in images:
     art_img = Image.open(img_file)
     w, h = art_img.size
+    name = Path(img_file).name
+    if name in unused_srcs:
+        unused_srcs.remove(name)
+    if name not in srcs and 'painting' in img_file and name != 'back.png':
+        missing_srcs.add(name)
     if os.path.exists(img_file + '.mcmeta'):
         # Assuming all animated images are square
         h = w
@@ -43,6 +61,11 @@ for img_file in images:
         frames += 5 * (len(art_imgs),)
         art_imgs.append((img_file, (w, h), art_img))
         max_size = max(w, h, max_size)
+
+if unused_srcs:
+    print(f'WARNING: nonexistent paintings from index.html: {unused_srcs}')
+if missing_srcs:
+    print(f'WARNING: paintings not in index.html: {missing_srcs}')
 
 art_imgs.sort(key=lambda desc: (desc[1], desc[0]))
 
@@ -70,3 +93,5 @@ anim_json = {
 }
 with open(animation, 'w') as f:
     json.dump(anim_json, f, indent=2)
+
+sys.exit(-1 if missing_srcs or unused_srcs else 0)
